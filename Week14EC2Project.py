aa2cded94c31d80c37b9776fac4f3ed4977ef90a
Jason Li <jlee77711@outlook.com>
2023-04-12 21:04:45 +0000

    RunDevelopmentInstances

---
+++ Week14EC2Project.py
@@ -0,0 +1,25 @@
+import boto3
+
+
+ec2 = boto3.resource('ec2')
+
+    
+# create 3 instances for DEV
+dev = ec2.create_instances(
+    ImageId='ami-01a5f5bb7b408ee72',  #This AMI ID is from my AWS Cloud IDE origional EC2 AMI ID
+    InstanceType='t2.micro',
+    MaxCount= 3,
+    MinCount= 3,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name','Value': 'Development'},
+                {'Key': 'ENV','Value': 'Development'}
+                
+            ]
+        }
+    ]
+)
+
+print(dev)
