29051f43b311c4059c1b5a4843db296f1e37c1af
Jason Li <124201063+Jleeluit@users.noreply.github.com>
2023-04-13 16:43:26 -0400

    Update Week14EC2Project.py

--- Week14EC2Project.py
+++ Week14EC2Project.py
@@ -6,7 +6,7 @@ ec2 = boto3.resource('ec2')
     
 # create 3 instances for DEV
 dev = ec2.create_instances(
-    ImageId='ami-01a5f5bb7b408ee72',  #This AMI ID is from my AWS Cloud9 IDE origional EC2 AMI ID
+    ImageId='ami-01a5f5bb7b408ee72',  #Customize your own ami ID in order to run on your end
     InstanceType='t2.micro',
     MaxCount= 3,
     MinCount= 3,
