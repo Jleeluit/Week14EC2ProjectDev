2a9072140ef8d3c14c6aa120a1c431bcea2f09ec
Jason Li <124201063+Jleeluit@users.noreply.github.com>
2023-04-13 16:42:59 -0400

    Update Week14RunProductionInstances.py

--- Week14RunProductionInstances.py
+++ Week14RunProductionInstances.py
@@ -5,7 +5,7 @@ ec2 = boto3.resource('ec2')
 
 # create 3 intances for PROD
 prod = ec2.create_instances(
-    ImageId='ami-01a5f5bb7b408ee72',  #This AMI ID is from my AWS Cloud 9 IDE original EC2 AMI ID
+    ImageId='ami-01a5f5bb7b408ee72',  #Customize your own ami ID in order to run on your end
     InstanceType='t2.micro',
     MaxCount= 3,
     MinCount= 3,
